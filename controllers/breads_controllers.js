018ab0cff25cb64c549cf8a3db9dcacb4192bb8b
NoahKKA <noahhhhh.a@gmail.com>
2023-04-19 18:50:24 -0700

    sending index and show data

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -6,16 +6,23 @@ const Bread = require('../models/breads.js')
 breads.get('/', (req, res) => {
   res.render('index',
     {
-      breads: Bread
+      breads: Bread,
+      title: 'Index Page'
     }
   )
-  //res.send(Bread)
 })
 
 // SHOW
+// SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-  })
-  
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
+})
+
 
 module.exports = breads
--- views/index.jsx
+++ views/index.jsx
@@ -1,17 +1,24 @@
 const React = require('react')
 const Default = require('./layouts/deafult')
 
-function Index ({breads}) {
+function Index ({breads, title}) {
     return (
-      <Default>
+      <Default title={title}>
         <h2>Index Page</h2>
         {/* <p>I have {breads[0].name} bread!</p> */}
         <ul>
             {
-                breads.map((bread, index) => {
-                    return (<li></li>)
-                })
+            breads.map((bread, index)=> {
+                return (
+                <li key={index}>
+                    <a href={`/breads/${index}`}>
+                    {bread.name}
+                    </a>
+                </li>
+                )
+            })
             }
+
         </ul>
       </Default>
     )
